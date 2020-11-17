e7c71f8eaa6e71d6386bccc04ebd1f15131358d2
Builder212 <building2day@gmail.com>
2020-11-17 17:16:13 -0500

    Redid more files, still haven't tested any.

--- light/light.py
+++ light/light.py
@@ -1,63 +1,126 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from ADCDevice import *
 
 class LED:
-    def __init__(self, pin):
-        self.ledPin = pin
+    def __init__(self, pin, def=0):
+        self.pin = pin
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.ledPin, GPIO.OUT)
-        GPIO.output(self.ledPin, GPIO.LOW)
-        print ('LED using pin %d.'%self.ledPin)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.adc = ADCDevice()
+
+        if (adc.detectI2C(0x48)):
+            self.adc = PCF8591()
+            self.is_dim = True
+            self.power = GPIO.PWN(pin, 1000)
+
+            if def == 1:
+                return True
+        else:
+            self.is_dim = False
+            GPIO.output(self.pin, GPIO.LOW)
+
+            if def == 1:
+                return False
 
     def on(self):
-        GPIO.output(self.ledPin, GPIO.HIGH)
+        if self.is_dim == True:
+            self.power.start(0)
+            value = self.adc.analogRead(0)
+            self.power.ChangeDutyCycle(value*100/255)
+        else:
+            GPIO.output(self.pin, GPIO.HIGH)
+
+    def dim_update(self):
+        value = self.adc.analogRead(0)
+        self.power.ChangeDutyCycle(value*100/255)
 
     def off(self):
-        GPIO.output(self.ledPin, GPIO.LOW)
+        if self.is_dim == True:
+            adc.close()
+        else:
+            GPIO.output(self.pin, GPIO.LOW)
+            GPIO.cleanup()
 
 class RGB_LED:
-    def __init__(self, red_pin, green_pin, blue_pin):
+    def __init__(self, red_pin, green_pin, blue_pin, def=0):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(red_pin, GPIO.OUT)
         GPIO.setup(green_pin, GPIO.OUT)
         GPIO.setup(blue_pin, GPIO.OUT)
-        GPIO.output(red_pin, GPIO.OUT)
-        GPIO.output(green_pin, GPIO.OUT)
-        GPIO.output(blue_pin, GPIO.OUT)
+        self.adc = ADCDevice()
+
+        if (adc.detectI2C(0x48)):
+            self.adc = PCF8591()
+            self.is_dim = True
+
+            if def == 1:
+                return True
+        else:
+            self.is_dim = False
+            GPIO.output(red_pin, GPIO.OUT)       # read ADC value of 3 potentiometers
+            GPIO.output(green_pin, GPIO.OUT)
+            GPIO.output(blue_pin, GPIO.OUT)
+
+            if def == 1:
+                return False
+
         self.pwm_red = GPIO.PWM(red_pin, 2000)
         self.pwm_green = GPIO.PWM(green_pin, 2000)
         self.pwm_blue = GPIO.PWM(blue_pin, 2000)
+
     def on(self, red_val=0, green_val=0, blue_val=0):
         self.pwm_red.start(red_val)
         self.pwm_green.start(green_val)
         self.pwm_blue.start(blue_val)
+
     def set_color(self, red_val, green_val, blue_val):
         self.pwm_red.ChangeDutyCycle(red_val)
         self.pwm_green.ChangeDutyCycle(green_val)
         self.pwm_blue.ChangeDutyCycle(blue_val)
+
+    def dim_color(self):
+        red_val = self.adc.analogRead(0)
+        green_val = self.adc.analogRead(1)
+        blue_val = self.adc.analogRead(2)
+        self.pwm_red.ChangeDutyCycle(value_Red*100/255)
+        self.pwm_green.ChangeDutyCycle(value_Green*100/255)
+        self.pwm_blue.ChangeDutyCycle(value_Blue*100/255)
+
     def off(self):
+        if self.is_dim == True:
+            adc.close()
+
         self.pwm_red.stop()
         self.pwm_green.stop()
         self.pwm_blue.stop()
-
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     print("Program is starting ... \n")
     type = input("Are you using a normal LED or a RGB LED? (0-LED, 1-RGB LED) ")
     print("\n")
+
     if type == 0:
         pin = input("What pin is your LED going to be operated from? ")
         print("\n")
-        led = LED(pin)
+        led, dim = LED(pin, 1)
+
         try:
-            while True:
+            if dim == True:
                 led.on()
-                sleep(5)
-                led.off()
-                sleep(5)
+                led.dim_update()
+            else:
+                while True:
+                    led.on()
+                    sleep(5)
+                    led.off()
+                    sleep(5)
         except KeyboardInterrupt:
+            led.off()
             GPIO.cleanup()
             exit()
+
     elif type == 1:
         from random import randint
 
@@ -67,15 +130,19 @@ if __name__ == '__main__':
         print("\n")
         blue_pin = input("What pin is your blue LED going to be operated from? ")
         print("\n")
-        led = RGB_LED(red_pin, green_pin, blue_pin)
+        led, dim = RGB_LED(red_pin, green_pin, blue_pin, 1)
 
         try:
             led.on()
-            while True:
-                red = random.randit(0, 100)
-                green = random.randit(0, 100)
-                blue = random.randit(0, 100)
-                led.set_color(red, green, blue)
+            if dim == true:
+                while True:
+                    led.dim_color()
+            else:
+                while True:
+                    red = random.randit(0, 100)
+                    green = random.randit(0, 100)
+                    blue = random.randit(0, 100)
+                    led.set_color(red, green, blue)
         except:
             led.off()
             GPIO.cleanup()
---
+++ photoresistor/photoresistor.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+from ADCDevice import *
+
+class photresistor:
+    def __init__(self):
+        self.adc = ADCDevice()
+        if(self.adc.detectI2C(0x48)):
+            self.adc = PCF8591()
+
+    def read_value(self):
+        value = self.adc.analogRead(0)
+        return value
+
+    def destroy(self):
+        self.adc.close()
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    print ('Program is starting ... ')
+    photoresistor = photresistor()
+
+    try:
+        while True:
+            print(photoresistor.read_value())
+    except KeyboardInterrupt:
+        photoresistor.destroy()
