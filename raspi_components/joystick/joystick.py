77c4abfcdd9012a8f0d656976866803b385d9542
Builder212 <building2day@gmail.com>
2021-02-14 20:57:44 -0500

    Added Joystick class.

---
+++ raspi_components/joystick/joystick.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+from ..ADC.ADC import PCF
+from ..ADC.ADC_errors import ADCError
+
+class joystick:
+    """
+    This class is used to get the values from a joystick.
+    """
+    def __init__(self, xchn, ychn, zpin):
+        """
+        Initiates the Joystick class on the given two channels and one pin.
+        """
+        self.xChannel = xchn
+        self.yChannel = ychn
+        self.zPin = zpin
+
+        self.adc = PCF()
+
+        try:
+            self.adc.is_connected()
+        except ADCError:
+            raise JoystickError("Failed to connect to your ADC.")
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.zPin, GPIO.IN, GPIO.PUD_UP)
+
+    def read(self):
+        """
+        Reads the values of the x, y, and z axis of the joystick.
+        """
+        x = self.adc.read(self.xChannel)
+        y = self.adc.read(self.yChannel)
+        z = GPIO.input(self.zPin)
+        return x, y, z
+
+    def close():
+        """
+        closes the connection with the adc.
+        """
+        self.adc.close()
+
+if __name__ == '__main__':
+    print('Program is starting ... ')
+    pin = input("What pin is your joystick connected too? ")
+    print("\n")
+    joystick = joystick()
+
+    try:
+        while True:
+            print(joystick.read_joystick())
+    except KeyboardInterrupt:
+        joystick.destroy()
